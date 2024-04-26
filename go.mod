c69ca144c79330fbbe718d60521c21387fb5896e
mpavithran <mpavithran1998@gmail.com>
2024-04-26 14:08:44 +0530

    coding done

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mpavithran/verify-email
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"net"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord")
+	
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+	
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: could not read from input: %v\n", err)
+	}
+
+}
+
+func checkDomain(Domain string) {
+	var hasMX, hasSPF, hasDMARC bool
+	var spfRecord, dmarcRecord string
+	
+	mxRecord, err := net.LookupMX(Domain)
+	
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	if len(mxRecord) > 0 {
+		hasMX = true
+	}
+
+	txtRecords, err := net.LookupTXT(Domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n",err)
+	}
+	
+	for _, record := range txtRecords{
+		if strings.HasPrefix(record, "v=spf1"){
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + Domain)
+
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range dmarcRecords{
+		if strings.HasPrefix(record, "v=DMARC1"){
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v", hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+	
+}
