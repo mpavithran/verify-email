fc3975adee836e606b97310dff31d38b7ce31841
mpavithran <mpavithran1998@gmail.com>
2024-04-27 23:37:19 +0530

    restructure code

--- main.go
+++ main.go
@@ -4,24 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"os"
 	"net"
+	"os"
 	"strings"
 )
-
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord")
-	
-	for scanner.Scan() {
-		checkDomain(scanner.Text())
-	}
-	
-	if err := scanner.Err(); err != nil {
-		log.Fatal("Error: could not read from input: %v\n", err)
-	}
-
-}
 
 func checkDomain(Domain string) {
 	var hasMX, hasSPF, hasDMARC bool
@@ -67,4 +53,18 @@ func checkDomain(Domain string) {
 
 	fmt.Printf("%v, %v, %v, %v, %v", hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
 	
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord")
+	
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+	
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: could not read from input: %v\n", err)
+	}
+
 }
